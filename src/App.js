5509ea9284c667788bb2c97baf8ae68073b985b8
roshith <roshithprakash7@gmail.com>
2024-03-29 14:08:19 +0530

    Adding vercel insight package. 29-03-2024. Roshith.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import {
 } from 'react-router-dom'
 
 import { About, Contact, Gallery, GridBox, Landing, NotFound, Projects, SmartPower } from './pages';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/smartpower" element={<SmartPower />} />
-        <Route path="/projects/gridbox" element={<GridBox />} />
-        <Route path="/projects/gallery" element={<Gallery />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <SpeedInsights />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/smartpower" element={<SmartPower />} />
+          <Route path="/projects/gridbox" element={<GridBox />} />
+          <Route path="/projects/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
